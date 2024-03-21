4afda45b11415ef53a81c707f06a37f4a087f784
668599 <668599@nd.com.cn>
2024-03-21 18:33:21 +0800

    [feat]增加-脚本-测试代码

--- scripts/check-code.sh
+++ scripts/check-code.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # 检查代码风格
 
-
 #当前脚本文件所在路径的绝对路径
 currentPath=$(realpath "$(dirname "$0")")
 #项目绝对路径
 projectPath="$(dirname "${currentPath}")"
 echo -e "\e[92m项目路径 ${projectPath}\e[0m"
 
+#检查代码
 echo -e "\e[93m======检查代码... \e[0m"
 cd  "${projectPath}" || exit
 echo "gofmt..."
---
+++ scripts/test-code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 测试代码
+
+#当前脚本文件所在路径的绝对路径
+currentPath=$(realpath "$(dirname "$0")")
+#项目绝对路径
+projectPath="$(dirname "${currentPath}")"
+echo -e "\e[92m项目路径 ${projectPath}\e[0m"
+
+#测试代码
+echo -e "\e[93m======测试代码... \e[0m"
+# todo menglc
+#   调用所有测试用例
+echo -e "\e[92m======测试代码完成\e[0m"
