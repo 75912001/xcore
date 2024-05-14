c9884b01326b8240ef52eeb9bddb749e8a30fcdd
668599 <668599@nd.com.cn>
2024-05-14 14:43:16 +0800

    [refactor]:

--- lib/constants/constants.go
+++ lib/constants/constants.go
@@ -1,5 +1,5 @@
 package constants
 
 const (
-	BusChannelNumberDefault = 1000000 //总线 channel 数量-默认. 1000000 大约占用15.6MB
+	BusChannelCapacityDefault = 1000000 //总线 channel 容量-默认. 1000000 大约占用15.6MB
 )
--- lib/constants/desc.go
+++ lib/constants/desc.go
@@ -5,7 +5,7 @@ const (
 	Success     = "success"     // 成功
 	Failure     = "failure"     // 失败
 	Available   = "available"   // 可用
-	Unavailable = "Unavailable" // 不可用
+	Unavailable = "unavailable" // 不可用
 	Valid       = "valid"       // 有效
 	Invalid     = "invalid"     // 无效
 	Legal       = "legal"       // 合法
@@ -13,7 +13,7 @@ const (
 	Permitted   = "permitted"   // 允许的
 	Prohibited  = "prohibited"  // 禁止的
 	Existent    = "existent"    // 存在
-	NotExist    = "not exist"   // 不存在
+	NotExist    = "not-exist"   // 不存在
 	Expect      = "expect"      // 期望
 	Unexpected  = "unexpected"  // 不期望,意外的
 	Enable      = "enable"      // 启用
@@ -21,19 +21,22 @@ const (
 	Normal      = "normal"      // 正常
 	Abnormal    = "abnormal"    // 不正常
 
-	Timeout    = "time out"     // 超时
+	Timeout    = "time-out"     // 超时
 	Retry      = "retry"        // 重试
-	OutOfRange = "out of range" // 超出范围
+	OutOfRange = "out-of-range" // 超出范围
 	Parameter  = "parameter"    // 参数
 
-	GoroutinePanic = "goroutine panic" // 协程 panic
-	GoroutineDone  = "goroutine done"  // 协程结束
-	FunctionPanic  = "function panic"  // 函数 panic
-	FunctionDone   = "function done"   // 函数结束
+	GoroutinePanic = "goroutine-panic" // 协程 panic
+	GoroutineDone  = "goroutine-done"  // 协程结束
+	FunctionPanic  = "function-panic"  // 函数 panic
+	FunctionDone   = "function-done"   // 函数结束
 
 	Etcd    = "etcd"    // Etcd
 	Redis   = "redis"   // Redis
 	Mongodb = "mongodb" // Mongodb
+	Mysql   = "mysql"   // Mysql
+	Kafka   = "kafka"   // Kafka
+	Nats    = "nats"    // Nats
 
 	Unknown = "unknown" // 未知
 )
