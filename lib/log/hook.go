8df7d516b25dbf7ee8d5a4de50d6aa8441a3f6bc
668599 <668599@nd.com.cn>
2024-08-10 20:42:47 +0800

    [feat]增加-脚本-测试代码

--- lib/log/hook.go
+++ lib/log/hook.go
@@ -1,33 +1,33 @@
 package log
 
 import (
-    "github.com/pkg/errors"
-    libruntime "xcore/lib/runtime"
+	"github.com/pkg/errors"
+	libruntime "xcore/lib/runtime"
 )
 
 // Hook 钩子
 // 钩子是一个接口，它定义了x个日志级别的钩子函数
 // e.g.: 修改entry的内容, 将 entry 发送至其他目标地
 type Hook interface {
-    Levels() []int           // 需要hook的等级列表
-    Fire(entry *entry) error // 执行的方法
+	Levels() []int           // 需要hook的等级列表
+	Fire(entry *entry) error // 执行的方法
 }
 
 type LevelHookMap map[int][]Hook // key: 日志等级, value: 钩子
 
 // add 添加钩子
 func (p LevelHookMap) add(hook Hook) {
-    for _, level := range hook.Levels() {
-        p[level] = append(p[level], hook)
-    }
+	for _, level := range hook.Levels() {
+		p[level] = append(p[level], hook)
+	}
 }
 
 // fire 处理钩子
 func (p LevelHookMap) fire(entry *entry) error {
-    for _, hook := range p[entry.level] {
-        if err := hook.Fire(entry); err != nil {
-            return errors.WithMessage(err, libruntime.Location())
-        }
-    }
-    return nil
+	for _, hook := range p[entry.level] {
+		if err := hook.Fire(entry); err != nil {
+			return errors.WithMessage(err, libruntime.Location())
+		}
+	}
+	return nil
 }
