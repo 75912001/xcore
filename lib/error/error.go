1f6c073516fd638dcdd75d202f619410ba857a3b
668599 <668599@nd.com.cn>
2024-09-04 15:33:57 +0800

    [feat]完善代码

--- lib/error/error.go
+++ lib/error/error.go
@@ -13,59 +13,59 @@
 package error
 
 import (
-    "fmt"
-    "github.com/pkg/errors"
+	"fmt"
+	"github.com/pkg/errors"
 )
 
 // 对象
 type object struct {
-    code         uint32 // 码
-    name         string // 名称
-    desc         string // 描述 description
-    extraMessage string // 附加信息
-    extraError   error  // 附加错误
+	code         uint32 // 码
+	name         string // 名称
+	desc         string // 描述 description
+	extraMessage string // 附加信息
+	extraError   error  // 附加错误
 }
 
 // 错误信息
 func (p *object) Error() string {
-    if Success.code == p.code {
-        return ""
-    }
-    return fmt.Sprintf("name:%v code:%v %#x description:%v extraMessage:%v extraError:%v",
-        p.name, p.code, p.code, p.desc, p.extraMessage, p.extraError)
+	if Success.code == p.code {
+		return ""
+	}
+	return fmt.Sprintf("name:%v code:%v %#x description:%v extraMessage:%v extraError:%v",
+		p.name, p.code, p.code, p.desc, p.extraMessage, p.extraError)
 }
 
 func (p *object) WithExtraMessage(extraMessage string) *object {
-    p.extraMessage = extraMessage
-    return p
+	p.extraMessage = extraMessage
+	return p
 }
 
 func (p *object) WithExtraError(extraError error) *object {
-    p.extraError = extraError
-    return p
+	p.extraError = extraError
+	return p
 }
 
 func (p *object) WithName(name string) *object {
-    p.name = name
-    return p
+	p.name = name
+	return p
 }
 
 func (p *object) WithDesc(desc string) *object {
-    p.desc = desc
-    return p
+	p.desc = desc
+	return p
 }
 
 // NewError 创建错误码,初始化程序的时候创建,创建失败会 panic.
 func NewError(code uint32) *object {
-    newError := newObject(code)
-    e := checkDuplication(newError)
-    if e != nil {
-        panic(
-            errors.WithMessage(e, fmt.Sprintf("new error object duplicates %v %#x", code, code)),
-        )
-    }
-    errMap[code] = struct{}{}
-    return newError
+	newError := newObject(code)
+	e := checkDuplication(newError)
+	if e != nil {
+		panic(
+			errors.WithMessage(e, fmt.Sprintf("new error object duplicates %v %#x", code, code)),
+		)
+	}
+	errMap[code] = struct{}{}
+	return newError
 }
 
 // 错误信息
@@ -74,14 +74,14 @@ var errMap = make(map[uint32]struct{})
 
 // 检查重复情况
 func checkDuplication(err *object) error {
-    if _, ok := errMap[err.code]; ok { // 重复
-        return errors.WithStack(errors.Errorf("duplicate err, code:%v %#x", err.code, err.code))
-    }
-    return nil
+	if _, ok := errMap[err.code]; ok { // 重复
+		return errors.WithStack(errors.Errorf("duplicate err, code:%v %#x", err.code, err.code))
+	}
+	return nil
 }
 
 func newObject(code uint32) *object {
-    return &object{
-        code: code,
-    }
+	return &object{
+		code: code,
+	}
 }
