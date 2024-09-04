bb0eac5f64984d9e7c6691f2a7bdfaa3567640ed
668599 <668599@nd.com.cn>
2024-09-04 15:44:19 +0800

    [feat]完善代码

--- lib/runtime/run.mode.go
+++ lib/runtime/run.mode.go
@@ -4,23 +4,23 @@ package runtime
 type RunMode uint32
 
 const (
-    RunModeRelease RunMode = 0 // release 模式
-    RunModeDebug   RunMode = 1 // debug 模式
+	RunModeRelease RunMode = 0 // release 模式
+	RunModeDebug   RunMode = 1 // debug 模式
 )
 
 // 程序运行模式
 var programRunMode = RunModeRelease
 
 func SetRunMode(mode RunMode) {
-    programRunMode = mode
+	programRunMode = mode
 }
 
 // IsDebug 是否为调试模式
 func IsDebug() bool {
-    return programRunMode == RunModeDebug
+	return programRunMode == RunModeDebug
 }
 
 // IsRelease 是否为发行模式
 func IsRelease() bool {
-    return programRunMode == RunModeRelease
+	return programRunMode == RunModeRelease
 }
