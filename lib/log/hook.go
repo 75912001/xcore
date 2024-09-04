9a62c86c689be99a1af41f241e0b9fbaad7391b2
668599 <668599@nd.com.cn>
2024-09-04 16:22:45 +0800

    [feat]完善代码

--- lib/log/hook.go
+++ lib/log/hook.go
@@ -9,11 +9,11 @@ import (
 // 钩子是一个接口，它定义了x个日志级别的钩子函数
 // e.g.: 修改entry的内容, 将 entry 发送至其他目标地
 type Hook interface {
-	Levels() []int           // 需要hook的等级列表
+	Levels() []uint32        // 需要hook的等级列表
 	Fire(entry *entry) error // 执行的方法
 }
 
-type LevelHookMap map[int][]Hook // key: 日志等级, value: 钩子
+type LevelHookMap map[uint32][]Hook // key: 日志等级, value: 钩子
 
 // add 添加钩子
 func (p LevelHookMap) add(hook Hook) {
