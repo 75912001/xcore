9296cafd295411147a59428dc82b1258433415ba
668599 <668599@nd.com.cn>
2024-09-04 16:07:09 +0800

    [feat]完善代码

--- lib/time/calculation.go
+++ lib/time/calculation.go
@@ -1,38 +1,38 @@
 package time
 
 import (
-    "strconv"
-    "time"
+	"strconv"
+	"time"
 )
 
 // GetDayStartTimestampFromTime 当天开始时间戳
 func (p *Mgr) GetDayStartTimestampFromTime(t *time.Time) int64 {
-    if p.utcSwitch.IsEnabled() {
-        return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).Unix()
-    }
-    return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Unix()
+	if p.utcSwitch.IsEnabled() {
+		return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).Unix()
+	}
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Unix()
 }
 
 // GetDayStartTimestampFromTimestamp 返回给定时间戳所在天的开始时间戳
 func (p *Mgr) GetDayStartTimestampFromTimestamp(timestamp int64) int64 {
-    if p.utcSwitch.IsEnabled() {
-        t := time.Unix(timestamp, 0).UTC()
-        return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).Unix()
-    }
-    t := time.Unix(timestamp, 0)
-    return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Unix()
+	if p.utcSwitch.IsEnabled() {
+		t := time.Unix(timestamp, 0).UTC()
+		return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, time.UTC).Unix()
+	}
+	t := time.Unix(timestamp, 0)
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Unix()
 }
 
 // GetYMDFromTimestamp 获取 e.g.:20210819
 //
 //	返回YMD
 func (p *Mgr) GetYMDFromTimestamp(timestamp int64) int {
-    var strYMD string
-    if p.utcSwitch.IsEnabled() {
-        strYMD = time.Unix(timestamp, 0).UTC().Format("20060102")
-    } else {
-        strYMD = time.Unix(timestamp, 0).Format("20060102")
-    }
-    ymd, _ := strconv.Atoi(strYMD)
-    return ymd
+	var strYMD string
+	if p.utcSwitch.IsEnabled() {
+		strYMD = time.Unix(timestamp, 0).UTC().Format("20060102")
+	} else {
+		strYMD = time.Unix(timestamp, 0).Format("20060102")
+	}
+	ymd, _ := strconv.Atoi(strYMD)
+	return ymd
 }
