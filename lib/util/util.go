a8a24879220aa9d44cec5c4be422937fad6f978a
668599 <668599@nd.com.cn>
2024-09-05 20:00:26 +0800

    [feat]完善代码

--- lib/util/util.go
+++ lib/util/util.go
@@ -3,9 +3,9 @@ package util
 import "unsafe"
 
 func IsLittleEndian() bool {
-    var i int32 = 0x01020304
-    u := unsafe.Pointer(&i)
-    pb := (*byte)(u)
-    b := *pb
-    return (b == 0x04)
+	var i int32 = 0x01020304
+	u := unsafe.Pointer(&i)
+	pb := (*byte)(u)
+	b := *pb
+	return (b == 0x04)
 }
