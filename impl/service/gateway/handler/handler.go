82b8832660170056004814c27e862c3deff2db46
668599 <668599@nd.com.cn>
2024-09-08 20:19:47 +0800

    [feat]完善代码

--- impl/service/gateway/handler/handler.go
+++ impl/service/gateway/handler/handler.go
@@ -1,27 +1,27 @@
 package handler
 
 import (
-    "context"
-    "fmt"
-    "google.golang.org/protobuf/proto"
-    protobufgateway "xcore/impl/protobuf/gateway"
-    xnetmessage "xcore/lib/net/message"
-    "xcore/lib/net/packet"
-    xruntime "xcore/lib/runtime"
-    xutil "xcore/lib/util"
+	"context"
+	"fmt"
+	"google.golang.org/protobuf/proto"
+	protobufgateway "xcore/impl/protobuf/gateway"
+	xnetmessage "xcore/lib/net/message"
+	"xcore/lib/net/packet"
+	xruntime "xcore/lib/runtime"
+	xutil "xcore/lib/util"
 )
 
 var GMessage xnetmessage.Mgr
 
 func init() {
-    GMessage.Register(protobufgateway.UserOnlineMsg_CMD,
-        xnetmessage.NewMessage().WithHandler(UserOnlineMsg).
-            WithNewProtoMessage(func() proto.Message { return new(protobufgateway.UserOnlineMsg) }).
-            WithStateSwitch(xutil.NewDefaultSwitch(true)))
+	GMessage.Register(protobufgateway.UserOnlineMsg_CMD,
+		xnetmessage.NewMessage().WithHandler(UserOnlineMsg).
+			WithNewProtoMessage(func() proto.Message { return new(protobufgateway.UserOnlineMsg) }).
+			WithStateSwitch(xutil.NewDefaultSwitch(true)))
 }
 
 func UserOnlineMsg(ctx context.Context, header packet.IHeader, message proto.Message, obj interface{}) error {
-    // todo menglc 处理用户上线
-    fmt.Println(ctx, header, message, obj, xruntime.Location())
-    return nil
+	// todo menglc 处理用户上线
+	fmt.Println(ctx, header, message, obj, xruntime.Location())
+	return nil
 }
