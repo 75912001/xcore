feea810d532725fad443fb3b2b01a74b2a904215
668599 <668599@nd.com.cn>
2024-09-08 16:02:19 +0800

    [feat]完善代码

--- lib/constants/etcd.go
+++ lib/constants/etcd.go
@@ -1,3 +1,8 @@
 package constants
 
 const EtcdTtlSecondDefault int64 = 33 // etcd 默认 TTL 时间 秒
+
+const EtcdWatchMsgTypeService string = "service"           // etcd watch 消息类型-服务
+const EtcdWatchMsgTypeCommand string = "command"           // etcd watch 消息类型-命令
+const EtcdWatchMsgTypeGM string = "gm"                     // etcd watch 消息类型-GM
+const EtcdWatchMsgTypeServiceBench string = "serviceBench" // etcd watch 消息类型-服务配置
