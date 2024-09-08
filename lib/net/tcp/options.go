636686cc6cf30b0fff52c47e526321daec9a3f5d
668599 <668599@nd.com.cn>
2024-09-08 17:04:21 +0800

    [feat]完善代码

--- lib/net/tcp/options.go
+++ lib/net/tcp/options.go
@@ -1,6 +1,6 @@
 package tcp
 
 type ConnOptions struct {
-    readBuffer  *int // readBuffer sets the size of the operating system's receive buffer associated with the connection.
-    writeBuffer *int // writeBuffer sets the size of the operating system's transmit buffer associated with the connection.
+	readBuffer  *int // readBuffer sets the size of the operating system's receive buffer associated with the connection.
+	writeBuffer *int // writeBuffer sets the size of the operating system's transmit buffer associated with the connection.
 }
