2f22ab4fa39fac8eafa457aa975fda10caf11746
668599 <668599@nd.com.cn>
2024-09-08 16:29:24 +0800

    [feat]完善代码

---
+++ lib/pprof/pprof.go
@@ -0,0 +1,30 @@
+package pprof
+
+import (
+    "net/http"
+    _ "net/http/pprof"
+    "runtime/debug"
+    xconstants "xcore/lib/constants"
+    xlog "xcore/lib/log"
+    xruntime "xcore/lib/runtime"
+)
+
+// StartHTTPprof 开启http采集分析
+//
+//	参数:
+//		addr: "0.0.0.0:8090"
+func StartHTTPprof(addr string) {
+    go func() {
+        defer func() {
+            if xruntime.IsRelease() {
+                if err := recover(); err != nil {
+                    xlog.PrintErr(xconstants.GoroutinePanic, err, xruntime.Location(), debug.Stack())
+                }
+            }
+            xlog.PrintInfo(xconstants.GoroutineDone)
+        }()
+        if err := http.ListenAndServe(addr, nil); err != nil {
+            xlog.PrintErr(xconstants.Failure, addr, err)
+        }
+    }()
+}
