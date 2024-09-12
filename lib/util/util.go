9476857beb2ea429739fe5f165f98fdf81b94a4c
668599 <668599@nd.com.cn>
2024-09-12 20:41:50 +0800

    [feat]完善代码

--- lib/util/util.go
+++ lib/util/util.go
@@ -1,6 +1,9 @@
 package util
 
-import "unsafe"
+import (
+	"net"
+	"unsafe"
+)
 
 func IsLittleEndian() bool {
 	var i int32 = 0x01020304
@@ -8,4 +11,11 @@ func IsLittleEndian() bool {
 	pb := (*byte)(u)
 	b := *pb
 	return b == 0x04
+}
+
+// IsNetErrorTemporary checks if a network error is temporary.
+// [NOTE] 不建议使用
+func IsNetErrorTemporary(err error) bool {
+	netErr, ok := err.(net.Error)
+	return ok && netErr.Temporary()
 }
