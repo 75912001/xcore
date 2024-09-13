36e665d306161d4040125657707883a7053c8da7
668599 <668599@nd.com.cn>
2024-09-13 14:59:45 +0800

    [feat]完善代码

---
+++ lib/net/tcp/handler.server.go
@@ -0,0 +1,32 @@
+package tcp
+
+import (
+	xerror "xcore/lib/error"
+)
+
+type DefaultHandlerServer struct {
+}
+
+func (p *DefaultHandlerServer) OnConnect(remote *DefaultRemote) error {
+	return xerror.NotImplemented
+}
+func (p *DefaultHandlerServer) OnCheckPacketLength(length uint32) error {
+	return xerror.NotImplemented
+}
+func (p *DefaultHandlerServer) OnCheckPacketLimit(remote *DefaultRemote) error {
+	return xerror.NotImplemented
+}
+func (p *DefaultHandlerServer) OnUnmarshalPacket(remote *DefaultRemote, data []byte) (*Packet, error) {
+	return nil, xerror.NotImplemented
+}
+func (p *DefaultHandlerServer) OnPacket(packet *Packet) error {
+	return xerror.NotImplemented
+}
+func (p *DefaultHandlerServer) OnDisconnect(remote *DefaultRemote) error {
+	return xerror.NotImplemented
+	// 下面是断开连接后需要做的事情
+	//if remote.IsConnect() {
+	//	remote.stop()
+	//}
+	//return nil
+}
