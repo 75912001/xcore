931704d88828d58271e86cf252266d22cd031d85
menglc <75912001@qq.com>
2024-09-19 02:24:05 +0800

    [feat]优化

--- lib/timer/milliSecond.go
+++ lib/timer/milliSecond.go
@@ -1,7 +1,7 @@
 package timer
 
-// millisecond 毫秒级定时器
-type millisecond struct {
+// Millisecond 毫秒级定时器
+type Millisecond struct {
 	Arg      interface{} // 参数
 	Function OnFun       // 超时调用的函数
 	expire   int64       // 过期时间
@@ -9,11 +9,11 @@ type millisecond struct {
 }
 
 // isValid 判断是否有效
-func (p *millisecond) isValid() bool {
+func (p *Millisecond) isValid() bool {
 	return p.valid
 }
 
-func (p *millisecond) reset() {
+func (p *Millisecond) reset() {
 	p.Arg = nil
 	p.Function = nil
 	p.expire = 0
@@ -25,12 +25,12 @@ func (p *millisecond) reset() {
 //	NOTE 必须与该timerOutChan线性处理.如:在同一个goroutine select中处理数据
 //	参数:
 //		毫秒定时器
-func DelMillisecond(t *millisecond) {
+func DelMillisecond(t *Millisecond) {
 	t.inValid()
 }
 
 // 设为无效
-func (p *millisecond) inValid() {
+func (p *Millisecond) inValid() {
 	p.Arg = nil
 	p.Function = nil
 	p.expire = 0
