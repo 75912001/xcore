a45bb4553e9cf884ecb1a26e1fd3189be8d4cddb
668599 <668599@nd.com.cn>
2024-09-20 19:38:04 +0800

    [feat]完善代码

--- lib/util/split.go
+++ lib/util/split.go
@@ -8,9 +8,9 @@ import (
 	xruntime "xcore/lib/runtime"
 )
 
-// Split2Uint32 拆分字符串, 返回 uint32 类型的 slice
+// Split2SliceU32 拆分字符串, 返回 uint32 类型的 slice
 // e.g.: "1,2,3" => []uint32{1, 2, 3}
-func Split2Uint32(s string, sep string) (u32Slice []uint32, err error) {
+func Split2SliceU32(s string, sep string) (u32Slice []uint32, err error) {
 	if 0 == len(s) {
 		return u32Slice, nil
 	}
@@ -26,11 +26,11 @@ func Split2Uint32(s string, sep string) (u32Slice []uint32, err error) {
 	return u32Slice, nil
 }
 
-// Split2Map 拆分字符串, 返回key为uint32类型、val为int64类型的map
+// Split2MapU32U32 拆分字符串, 返回key为uint32类型、val为uint32类型的map
 // e.g.: "1,2;3,4" => map[uint32]int64{1:2, 3:4}
-func Split2Map(s string, sep1 string, sep2 string) (map[uint32]int64, error) {
+func Split2MapU32U32(s string, sep1 string, sep2 string) (map[uint32]uint32, error) {
 	slice := strings.Split(s, sep1)
-	m := make(map[uint32]int64)
+	m := make(map[uint32]uint32)
 	var err error
 	for _, v := range slice {
 		if 0 == len(v) {
@@ -40,15 +40,15 @@ func Split2Map(s string, sep1 string, sep2 string) (map[uint32]int64, error) {
 		if len(sliceAttr) != 2 {
 			return nil, errors.WithMessage(xerror.Param, xruntime.Location())
 		}
-		var idUint64 uint64
-		var valInt64 int64
-		if idUint64, err = strconv.ParseUint(sliceAttr[0], 10, 32); err != nil {
+		var id uint64
+		var val uint64
+		if id, err = strconv.ParseUint(sliceAttr[0], 10, 32); err != nil {
 			return nil, errors.WithMessage(err, xruntime.Location())
 		}
-		if valInt64, err = strconv.ParseInt(sliceAttr[1], 10, 32); err != nil {
+		if val, err = strconv.ParseUint(sliceAttr[1], 10, 32); err != nil {
 			return nil, errors.WithMessage(err, xruntime.Location())
 		}
-		m[uint32(idUint64)] = valInt64
+		m[uint32(id)] = uint32(val)
 	}
 	return m, nil
 }
