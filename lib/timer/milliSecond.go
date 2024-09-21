c0884e072d1878e4cfe4f014e8c40e2ff00259f4
668599 <668599@nd.com.cn>
2024-09-21 18:47:32 +0800

    [feat]完善代码

--- lib/timer/milliSecond.go
+++ lib/timer/milliSecond.go
@@ -1,38 +1,25 @@
 package timer
 
+import xutil "xcore/lib/util"
+
 // Millisecond 毫秒级定时器
 type Millisecond struct {
-	Arg      interface{} // 参数
-	Function OnFun       // 超时调用的函数
-	expire   int64       // 过期时间
-	valid    bool        // 有效(false:不执行,扫描时自动删除)
-}
-
-// isValid 判断是否有效
-func (p *Millisecond) isValid() bool {
-	return p.valid
+	xutil.ICallBackFunc       // 回调
+	xutil.ISwitch             // 有效(false:不执行,扫描时自动删除)
+	expire              int64 // 过期时间
 }
 
 func (p *Millisecond) reset() {
-	p.Arg = nil
-	p.Function = nil
+	p.ICallBackFunc = nil
+	p.ISwitch.Enable()
 	p.expire = 0
-	p.valid = false
 }
 
 // DelMillisecond 删除毫秒级定时器
 //
-//	NOTE 必须与该timerOutChan线性处理.如:在同一个goroutine select中处理数据
+//	[NOTE] 必须与该 outgoingTimeoutChan 线性处理.如:在同一个 goroutine select 中处理数据
 //	参数:
 //		毫秒定时器
 func DelMillisecond(t *Millisecond) {
-	t.inValid()
-}
-
-// 设为无效
-func (p *Millisecond) inValid() {
-	p.Arg = nil
-	p.Function = nil
-	p.expire = 0
-	p.valid = false
+	t.Disable()
 }
