ee798b9c19f4cbd9f3ec3b33b3b9f855d3c2297e
668599 <668599@nd.com.cn>
2024-09-21 20:42:36 +0800

    [feat]完善代码

--- lib/log/entry.go
+++ lib/log/entry.go
@@ -18,11 +18,10 @@ type entry struct {
 	level        uint32    // 本条目的日志级别
 	time         time.Time // 生成日志的时间
 	callerInfo   string    // 调用堆栈信息
-	message      string    // 日志消息
+	message      string    // 日志信息
 	ctx          context.Context
 	extendFields extendFields // [string,interface{}] key,value;key,value...
-
-	outString string // 输出的字符串
+	outString    string       // 输出的字符串
 }
 
 func newEntry() *entry {
@@ -35,6 +34,7 @@ func (p *entry) reset() {
 	p.message = ""
 	p.ctx = nil
 	p.extendFields = nil
+	p.outString = ""
 }
 
 func withLevel(p *entry, level uint32) *entry {
