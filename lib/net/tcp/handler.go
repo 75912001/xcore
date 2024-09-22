e7105cf1819b63791cfceec3477145c8f485a4f1
668599 <668599@nd.com.cn>
2024-09-22 17:30:03 +0800

    [feat]完善代码

--- lib/net/tcp/handler.go
+++ lib/net/tcp/handler.go
@@ -2,7 +2,7 @@ package tcp
 
 // IHandler 处理 接口
 type IHandler interface {
-	OnConnect(remote *DefaultRemote) error                                        // 处理-链接成功
+	OnConnect(remote *DefaultRemote) error                                        // 处理-链接成功-对方链接过来
 	OnCheckPacketLength(length uint32) error                                      // 处理-检查长度是否合法(包头中)
 	OnCheckPacketLimit(remote *DefaultRemote) error                               // 处理-限流
 	OnUnmarshalPacket(remote *DefaultRemote, data []byte) (*DefaultPacket, error) // 处理-数据包-反序列化 // data:数据 [NOTE] 如果保存该参数 则 需要copy
