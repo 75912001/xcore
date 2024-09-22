9582b882e2a4a259d56083c70fac726a073ee2f4
668599 <668599@nd.com.cn>
2024-09-22 18:11:55 +0800

    [feat]完善代码

--- lib/net/tcp/packet.go
+++ lib/net/tcp/packet.go
@@ -10,7 +10,6 @@ import (
 
 // DefaultPacket 数据包
 type DefaultPacket struct {
-	Remote          *DefaultRemote
 	Header          packet.IHeader // 包头
 	Message         proto.Message  // 解析出的数据
 	PassThroughData []byte         // 包体数据(不带包头)
@@ -19,16 +18,18 @@ type DefaultPacket struct {
 }
 
 func (p *DefaultPacket) Marshal() (data []byte, err error) {
+	// todo menglc 序列化
 	return nil, xerror.NotImplemented
 }
 
 func (p *DefaultPacket) Unmarshal(data []byte) (header packet.IHeader, message proto.Message, err error) {
+	// todo menglc 反序列化
 	return nil, nil, xerror.NotImplemented
 }
 
 // IsPassThrough 是否透传
 // 是: DefaultPacket.PassThroughBody 可用
 // 否: DefaultPacket.Message 可用
-func (p *DefaultPacket) IsPassThrough() bool {
-	return p.PassThroughData != nil
-}
+//func (p *DefaultPacket) IsPassThrough() bool {
+//	return p.PassThroughData != nil
+//}
