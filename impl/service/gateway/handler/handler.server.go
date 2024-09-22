3b9e9c36948e3640ceca519f796f5e36fb9b0c99
668599 <668599@nd.com.cn>
2024-09-22 19:44:02 +0800

    [feat]完善代码

---
+++ impl/service/gateway/handler/handler.server.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	xnettcp "xcore/lib/net/tcp"
+)
+
+type Server struct {
+	*xnettcp.DefaultHandlerServer
+}
+
+func NewServer() *Server {
+	return &Server{
+		DefaultHandlerServer: xnettcp.NewDefaultHandlerServer(),
+	}
+}
+
+func (p *Server) OnConnect(remote *xnettcp.DefaultRemote) error {
+	return nil
+}
+func (p *Server) OnCheckPacketLength(length uint32) error {
+	return nil
+}
+func (p *Server) OnCheckPacketLimit(remote *xnettcp.DefaultRemote) error {
+	return nil
+}
+func (p *Server) OnUnmarshalPacket(remote *xnettcp.DefaultRemote, data []byte) (*xnettcp.DefaultPacket, error) {
+	return nil, nil
+}
+func (p *Server) OnPacket(packet *xnettcp.DefaultPacket) error {
+	return nil
+}
+func (p *Server) OnDisconnect(remote *xnettcp.DefaultRemote) error {
+	// todo menglc
+
+	if err := p.DefaultHandlerServer.OnDisconnect(remote); err != nil {
+		return err
+	}
+	return nil
+}
