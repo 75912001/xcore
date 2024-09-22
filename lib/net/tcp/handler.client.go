4324bd68a4643da7bf2a132eb91e000bbacd15f4
668599 <668599@nd.com.cn>
2024-09-22 17:37:41 +0800

    [feat]完善代码

--- lib/net/tcp/handler.client.go
+++ lib/net/tcp/handler.client.go
@@ -17,16 +17,19 @@ func (p *DefaultHandlerClient) OnCheckPacketLimit(_ *DefaultRemote) error {
 	return nil
 }
 func (p *DefaultHandlerClient) OnUnmarshalPacket(remote *DefaultRemote, data []byte) (*DefaultPacket, error) {
+	// todo menglc 实现解包
 	return nil, xerror.NotImplemented
 }
 func (p *DefaultHandlerClient) OnPacket(packet *DefaultPacket) error {
+	// todo menglc 实现处理数据包
 	return xerror.NotImplemented
 }
 func (p *DefaultHandlerClient) OnDisconnect(remote *DefaultRemote) error {
+	// todo menglc 实现断开连接
 	return xerror.NotImplemented
 	// 下面是断开连接后需要做的事情
 	//if remote.IsConnect() {
-	//	remote.stop()
+	//	remote.Stop()
 	//}
 	//return nil
 }
