2fcd08df6c7770500a2cc921c15188c6ae30afe9
668599 <668599@nd.com.cn>
2024-09-22 14:14:08 +0800

    [feat]完善代码

---
+++ lib/log/level.subscribe.go
@@ -0,0 +1,15 @@
+package log
+
+type levelSubscribe struct {
+	subMap       map[uint32]struct{}
+	callBackFunc CallBackFunc
+}
+
+// CallBackFunc 回调函数
+type CallBackFunc func(level uint32, outString string)
+
+// 是否订阅
+func (p *levelSubscribe) isSubscribe(level uint32) bool {
+	_, ok := p.subMap[level]
+	return ok
+}
