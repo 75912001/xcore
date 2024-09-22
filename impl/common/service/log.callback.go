1a0357072f8bfe0791b92548d9ee806df1eb53f1
668599 <668599@nd.com.cn>
2024-09-22 19:30:32 +0800

    [feat]完善代码

---
+++ impl/common/service/log.callback.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"fmt"
+	xruntime "xcore/lib/runtime"
+)
+
+func logCallBackFunc(level uint32, outString string) {
+	if xruntime.IsDebug() {
+		fmt.Println(level, outString)
+	}
+	return
+}
