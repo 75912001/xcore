13aa97a4b2d2861b70c486a88cb4c938aa6d9d27
668599 <668599@nd.com.cn>
2024-09-25 16:13:25 +0800

    [feat]完善代码

--- lib/example/main/timer.go
+++ lib/example/main/timer.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 	"time"
+	xutil "xcore/lib/callback"
 	xconstants "xcore/lib/constants"
 	xtimer "xcore/lib/timer"
-	xutil "xcore/lib/util"
 )
 
 func cbSecond(arg interface{}) error {
@@ -47,7 +47,7 @@ func exampleTimer() {
 			switch t := v.(type) {
 			case addSecondSignal:
 				for i := 0; i < 10; i++ {
-					defaultCallBack := xutil.NewDefaultCallBack(cbSecond, uint64(i))
+					defaultCallBack := xutil.NewCallBack(cbSecond, uint64(i))
 					second := timer.AddSecond(defaultCallBack, time.Now().Unix()+int64(i))
 					switch i {
 					case 3, 7, 9:
@@ -57,7 +57,7 @@ func exampleTimer() {
 				}
 			case addMillisecondSignal:
 				for i := 0; i < 10000; i += 1000 {
-					defaultCallBack := xutil.NewDefaultCallBack(cbMillisecond, uint64(i))
+					defaultCallBack := xutil.NewCallBack(cbMillisecond, uint64(i))
 					millisecond := timer.AddMillisecond(defaultCallBack, time.Now().UnixMilli()+int64(i))
 					switch i {
 					case 3000, 7000, 9000:
@@ -66,9 +66,9 @@ func exampleTimer() {
 					}
 				}
 			case xtimer.EventTimerSecond:
-				_ = t.CallBackFunc()
+				_ = t.Function()
 			case xtimer.EventTimerMillisecond:
-				_ = t.CallBackFunc()
+				_ = t.Function()
 			}
 		}
 	}
