80d7e64eb30ffa4a3f8e37299600d28d3162dc92
668599 <668599@nd.com.cn>
2024-09-26 21:18:33 +0800

    [feat]完善代码

---
+++ lib/net/tcp/send.go
@@ -0,0 +1,23 @@
+package tcp
+
+import (
+	"github.com/pkg/errors"
+	xerror "xcore/lib/error"
+	xnetpacket "xcore/lib/net/packet"
+	xruntime "xcore/lib/runtime"
+)
+
+type ISend interface {
+	Send(packet xnetpacket.IPacket) error
+}
+
+type defaultSend struct {
+}
+
+func NewDefaultSend() ISend {
+	return &defaultSend{}
+}
+
+func (p *defaultSend) Send(packet xnetpacket.IPacket) error {
+	return errors.WithMessage(xerror.NotImplemented, xruntime.Location())
+}
