d3f242f6eafee206ebd21c8bb394fa367395e479
668599 <668599@nd.com.cn>
2024-09-29 20:37:09 +0800

    [feat]完善代码

--- lib/net/tcp/event.go
+++ lib/net/tcp/event.go
@@ -1,70 +1,13 @@
 package tcp
 
 import (
-	"github.com/pkg/errors"
-	xerror "xcore/lib/error"
-	xlog "xcore/lib/log"
 	xnetpacket "xcore/lib/net/packet"
-	xruntime "xcore/lib/runtime"
 )
 
 type IEvent interface {
 	Connect(handler IHandler, remote IRemote) error                           // 链接 放入 事件中
 	Disconnect(handler IHandler, remote IRemote) error                        // 断开链接 放入 事件中
 	Packet(handler IHandler, remote IRemote, packet xnetpacket.IPacket) error // 数据包 放入 事件中
-}
-
-type defaultEvent struct {
-	eventChan chan<- interface{} // 待处理的事件
-}
-
-func newDefaultEvent(eventChan chan<- interface{}) IEvent {
-	return &defaultEvent{
-		eventChan: eventChan,
-	}
-}
-
-// Connect 连接
-func (p *defaultEvent) Connect(handler IHandler, remote IRemote) error {
-	select {
-	case p.eventChan <- &EventConnect{
-		IHandler: handler,
-		IRemote:  remote,
-	}:
-	default:
-		xlog.PrintfErr("push EventConnect failed with eventChan full. remote:%v", remote)
-		return errors.WithMessage(xerror.ChannelFull, xruntime.Location())
-	}
-	return nil
-}
-
-// Disconnect 断开链接
-func (p *defaultEvent) Disconnect(handler IHandler, remote IRemote) error {
-	select {
-	case p.eventChan <- &EventDisconnect{
-		IHandler: handler,
-		IRemote:  remote,
-	}:
-	default:
-		xlog.PrintfErr("push EventDisconnect failed with eventChan full. remote:%v", remote)
-		return errors.WithMessage(xerror.ChannelFull, xruntime.Location())
-	}
-	return nil
-}
-
-// Packet 数据包
-func (p *defaultEvent) Packet(handler IHandler, remote IRemote, packet xnetpacket.IPacket) error {
-	select {
-	case p.eventChan <- &EventPacket{
-		IHandler: handler,
-		IRemote:  remote,
-		IPacket:  packet,
-	}:
-	default:
-		xlog.PrintfErr("push EventPacket failed with eventChan full. remote:%v packet:%v", remote, packet)
-		return errors.WithMessage(xerror.ChannelFull, xruntime.Location())
-	}
-	return nil
 }
 
 // EventDisconnect 事件-断开链接
