414831418e64962590d81e12f0e26c5e058db2b7
668599 <668599@nd.com.cn>
2024-09-30 14:40:03 +0800

    [feat]完善代码

--- lib/net/connect/connect.options.go
+++ lib/net/connect/connect.options.go
@@ -1,6 +1,14 @@
-package tcp
+package connect
 
-type connOptions struct {
+type ConnOptions struct {
 	readBuffer  *int // readBuffer sets the size of the operating system's receive buffer associated with the connection. [default]: 系统默认
 	writeBuffer *int // writeBuffer sets the size of the operating system's transmit buffer associated with the connection. [default]: 系统默认
+}
+
+// NewConnOptions 新的ConnOptions
+func NewConnOptions() *ConnOptions {
+	return &ConnOptions{
+		readBuffer:  nil,
+		writeBuffer: nil,
+	}
 }
