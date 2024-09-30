a155c088ed5e015efb59de26d85af87bbe3ebc9f
668599 <668599@nd.com.cn>
2024-09-30 14:48:18 +0800

    [feat]完善代码

--- lib/net/event/event.default.go
+++ lib/net/event/event.default.go
@@ -1,10 +1,12 @@
-package tcp
+package event
 
 import (
 	"github.com/pkg/errors"
 	xerror "xcore/lib/error"
 	xlog "xcore/lib/log"
+	xnethandler "xcore/lib/net/handler"
 	xnetpacket "xcore/lib/net/packet"
+	xnetremote "xcore/lib/net/remote"
 	xruntime "xcore/lib/runtime"
 )
 
@@ -12,50 +14,50 @@ type defaultEvent struct {
 	eventChan chan<- interface{} // 待处理的事件
 }
 
-func newDefaultEvent(eventChan chan<- interface{}) IEvent {
+func NewDefaultEvent(eventChan chan<- interface{}) IEvent {
 	return &defaultEvent{
 		eventChan: eventChan,
 	}
 }
 
 // Connect 连接
-func (p *defaultEvent) Connect(handler IHandler, remote IRemote) error {
+func (p *defaultEvent) Connect(handler xnethandler.IHandler, remote xnetremote.IRemote) error {
 	select {
-	case p.eventChan <- &EventConnect{
+	case p.eventChan <- &Connect{
 		IHandler: handler,
 		IRemote:  remote,
 	}:
 	default:
-		xlog.PrintfErr("push EventConnect failed with eventChan full. remote:%v", remote)
+		xlog.PrintfErr("push Connect failed with eventChan full. remote:%v", remote)
 		return errors.WithMessage(xerror.ChannelFull, xruntime.Location())
 	}
 	return nil
 }
 
 // Disconnect 断开链接
-func (p *defaultEvent) Disconnect(handler IHandler, remote IRemote) error {
+func (p *defaultEvent) Disconnect(handler xnethandler.IHandler, remote xnetremote.IRemote) error {
 	select {
-	case p.eventChan <- &EventDisconnect{
+	case p.eventChan <- &Disconnect{
 		IHandler: handler,
 		IRemote:  remote,
 	}:
 	default:
-		xlog.PrintfErr("push EventDisconnect failed with eventChan full. remote:%v", remote)
+		xlog.PrintfErr("push Disconnect failed with eventChan full. remote:%v", remote)
 		return errors.WithMessage(xerror.ChannelFull, xruntime.Location())
 	}
 	return nil
 }
 
 // Packet 数据包
-func (p *defaultEvent) Packet(handler IHandler, remote IRemote, packet xnetpacket.IPacket) error {
+func (p *defaultEvent) Packet(handler xnethandler.IHandler, remote xnetremote.IRemote, packet xnetpacket.IPacket) error {
 	select {
-	case p.eventChan <- &EventPacket{
+	case p.eventChan <- &Packet{
 		IHandler: handler,
 		IRemote:  remote,
 		IPacket:  packet,
 	}:
 	default:
-		xlog.PrintfErr("push EventPacket failed with eventChan full. remote:%v packet:%v", remote, packet)
+		xlog.PrintfErr("push Packet failed with eventChan full. remote:%v packet:%v", remote, packet)
 		return errors.WithMessage(xerror.ChannelFull, xruntime.Location())
 	}
 	return nil
