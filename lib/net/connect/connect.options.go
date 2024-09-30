af3e52806dda1fcee7398dc7c191ee79dda07c3e
668599 <668599@nd.com.cn>
2024-09-30 14:59:10 +0800

    [feat]完善代码

--- lib/net/connect/connect.options.go
+++ lib/net/connect/connect.options.go
@@ -1,14 +1,14 @@
 package connect
 
 type ConnOptions struct {
-	readBuffer  *int // readBuffer sets the size of the operating system's receive buffer associated with the connection. [default]: 系统默认
-	writeBuffer *int // writeBuffer sets the size of the operating system's transmit buffer associated with the connection. [default]: 系统默认
+	ReadBuffer  *int // readBuffer sets the size of the operating system's receive buffer associated with the connection. [default]: 系统默认
+	WriteBuffer *int // writeBuffer sets the size of the operating system's transmit buffer associated with the connection. [default]: 系统默认
 }
 
 // NewConnOptions 新的ConnOptions
 func NewConnOptions() *ConnOptions {
 	return &ConnOptions{
-		readBuffer:  nil,
-		writeBuffer: nil,
+		ReadBuffer:  nil,
+		WriteBuffer: nil,
 	}
 }
