3ab221b9604a982526cae2f55de22b5ccf385744
menglingchao <75912001@qq.com>
2025-03-20 12:08:38 +0800

    g

---
+++ impl/service/gateway/user.connect.go
@@ -0,0 +1,21 @@
+package gateway
+
+import xnettcp "xcore/lib/net/tcp"
+import xnetconnect "xcore/lib/net/connect"
+
+// UserConnect 用户-链接
+type UserConnect struct {
+	IRemote          xnettcp.IRemote
+	ExpireTimestamp  int64  // 到期时间戳-灰度
+	heartbeatRandom  uint64 // 心跳随机数
+	heartbeatTimeout int64  // 心跳超时时间
+	Status           *xnetconnect.Status
+}
+
+func newUserConnect(remote xnettcp.IRemote) *UserConnect {
+	return &UserConnect{
+		IRemote:          remote,
+		heartbeatTimeout: gService.TimeMgr.ShadowTimestamp() + UserHeartbeatIntervalMax,
+		Status:           xnetconnect.NewStatus(),
+	}
+}
